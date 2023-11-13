a3cbd3436b0a8e9a49fec6aa8727e9025704fdb6
jacobtuxen <jacob.tuxen@outlook.com>
2023-11-13 14:18:59 +0100

    Update unet_model.py

--- UNET3D/unet_model/unet_model.py
+++ UNET3D/unet_model/unet_model.py
@@ -10,29 +10,23 @@ class UNet3D(nn.Module):
         self.n_classes = n_classes
         self.trilinear = trilinear
 
-        self.batchnorm = nn.BatchNorm3d(n_channels)
-        self.inc = (DoubleConv(n_channels, 64))
-        self.down1 = (Down(64, 128))
-        self.down2 = (Down(128, 256))
-        self.down3 = (Down(256, 512))
+        self.inc = (DoubleConv(n_channels, 32))
+        self.down1 = (Down(32, 64))
+        self.down2 = (Down(64, 128))
+        self.down3 = (Down(128, 256))
         factor = 2 if trilinear else 1
-        self.down4 = (Down(512, 1024 // factor))
-        self.up1 = (Up(1024, 512 // factor, trilinear))
-        self.up2 = (Up(512, 256 // factor, trilinear))
-        self.up3 = (Up(256, 128 // factor, trilinear))
-        self.up4 = (Up(128, 64, trilinear))
-        self.outc = (OutConv(64, n_classes))
+        self.up1 = (Up(256, 128 // factor, trilinear))
+        self.up2 = (Up(128, 64 // factor, trilinear))
+        self.up3 = (Up(64, 32, trilinear))
+        self.outc = (OutConv(32, n_classes))
 
     def forward(self, x):
-        x1 = self.batchnorm(x)
         x1 = self.inc(x)
         x2 = self.down1(x1)
         x3 = self.down2(x2)
         x4 = self.down3(x3)
-        x5 = self.down4(x4)
-        x = self.up1(x5, x4)
-        x = self.up2(x, x3)
-        x = self.up3(x, x2)
-        x = self.up4(x, x1)
+        x = self.up1(x4, x3)
+        x = self.up2(x, x2)
+        x = self.up3(x, x1)
         logits = self.outc(x)
         return logits
