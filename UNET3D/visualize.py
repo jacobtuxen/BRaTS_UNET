f16ea3ef9273c16c7df238cd555490c4f27221a6
chrvalentin <s211469@student.dtu.dk>
2023-11-20 13:36:38 +0100

    Changed plots

--- UNET3D/visualize.py
+++ UNET3D/visualize.py
@@ -15,21 +15,31 @@ def visualize_model_output(epoch, input, model, patient_id, device):
     input_images = input.cpu().numpy()
 
   input_images = np.squeeze(input_images, axis=0)
-  fig, ax = plt.subplots(len(slices), 6, figsize=(15, 5))
+  fig, ax = plt.subplots(len(slices), 5, figsize=(15, 5))
   seg = [nib.load(f'/work3/s194572/data/{patient_id}/{patient_id}_{titles}').get_fdata() for titles in ['seg.nii']]
 
   for i, slice in enumerate(slices):
     for j in range(len(plot_titles)):
       if j == 0:
         ax[i, j].imshow(input_images[0, :, :, int(slice)], cmap='gray')
+        ax[i,j].set_xticks([])
+        ax[i,j].set_yticks([])
       elif j == 1:
         ax[i, j].imshow(input_images[1, :, :, int(slice)], cmap='gray')
+        ax[i,j].set_xticks([])
+        ax[i,j].set_yticks([])
       elif j == 2:
         ax[i, j].imshow(input_images[2, :, :, int(slice)], cmap='gray')
+        ax[i,j].set_xticks([])
+        ax[i,j].set_yticks([])
       elif j == 3:
         ax[i, j].imshow(seg[0][:, :, int(slice)], cmap='gray')
+        ax[i,j].set_xticks([])
+        ax[i,j].set_yticks([])
       elif j == 4:
         ax[i, j].imshow(output[0, :, :, int(slice)], cmap='gray')
+        ax[i,j].set_xticks([])
+        ax[i,j].set_yticks([])
       if i==0:
         ax[i, j].set_title(f'{plot_titles[j]}')
       
